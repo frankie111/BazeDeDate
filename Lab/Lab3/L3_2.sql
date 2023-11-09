f307ff0f7d72a626abb0114d69f066301dd9897c
Mircox <mircea.rautoiu@gmail.com>
2023-11-09 20:46:38 +0200

    Add scaffolding for GoToVersion

--- Lab/Lab3/L3_2.sql
+++ Lab/Lab3/L3_2.sql
@@ -1,4 +1,4 @@
-CREATE PROCEDURE SetUp
+CREATE OR ALTER PROCEDURE SetUp
 AS
 BEGIN
 	CREATE TABLE CurrentVersion (CurrentVersion INT PRIMARY KEY);
@@ -20,7 +20,7 @@ END;
 
 GO
 
-CREATE PROCEDURE RevertSetUp
+CREATE OR ALTER PROCEDURE RevertSetUp
 AS
 BEGIN
 	DROP TABLE CurrentVersion;
@@ -30,7 +30,7 @@ END;
 
 GO
 
-CREATE PROCEDURE CreateTable(
+CREATE OR ALTER PROCEDURE CreateTable(
 	@tableName VARCHAR(128),
 	@columns VARCHAR(MAX),
 	@addToVersionHistory BIT = 1
@@ -56,7 +56,7 @@ END;
 
 GO
 
-CREATE PROCEDURE RevertCreateTable(
+CREATE OR ALTER PROCEDURE RevertCreateTable(
 	@tableName VARCHAR(128)
 ) AS
 BEGIN
@@ -70,7 +70,7 @@ END;
 
 GO
 
-CREATE PROCEDURE AddColumn(
+CREATE OR ALTER PROCEDURE AddColumn(
 	@tableName VARCHAR(128),
 	@columnName VARCHAR(128),
 	@columnType VARCHAR(128),
@@ -98,7 +98,7 @@ END;
 
 GO
 
-CREATE PROCEDURE RevertAddColumn(
+CREATE OR ALTER PROCEDURE RevertAddColumn(
 	@tableName VARCHAR(128),
 	@columnName VARCHAR(128)
 ) AS
@@ -111,7 +111,7 @@ END;
 
 GO
 
-CREATE PROCEDURE ChangeColumnType(
+CREATE OR ALTER PROCEDURE ChangeColumnType(
 	@tableName VARCHAR(128),
 	@columnName VARCHAR(128),
 	@newDataType VARCHAR(128),
@@ -138,7 +138,7 @@ END;
 
 GO
 
-CREATE PROCEDURE RevertChangeColumnType(
+CREATE OR ALTER PROCEDURE RevertChangeColumnType(
 	@tableName VARCHAR(128),
 	@columnName VARCHAR(128),
 	@oldDataType VARCHAR(128)
@@ -146,5 +146,132 @@ CREATE PROCEDURE RevertChangeColumnType(
 BEGIN
 	EXEC ChangeColumnType @tableName, @columnName, @oldDataType, 0;
 END;
+
+GO
+
+CREATE OR ALTER PROCEDURE CreateDefaultConstraint(
+	@tableName VARCHAR(128),
+	@columnName VARCHAR(128),
+	@defaultValue VARCHAR(128),
+	@addToVersionHistory BIT = 1
+) AS
+BEGIN
+	DECLARE @SQL VARCHAR(MAX);
+	SET @SQL = 'ALTER TABLE ' + @tableName + ' ADD CONSTRAINT DF_' + @tableName + '_' + 
+			   @columnName + ' DEFAULT ''' + @defaultValue + ''' FOR ' + @columnName;
+	PRINT @SQL;
+	EXEC (@SQL);
+
+	IF @addToVersionHistory = 1
+	BEGIN
+		INSERT INTO VersionHistory (ProcedureName) VALUES ('CreateDefaultConstraint');
+		DECLARE @versionId INT = SCOPE_IDENTITY();
+
+		INSERT INTO VersionParams (VersionId, ParamName, ParamValue) VALUES
+			(@versionId, 'tableName', @tableName),
+			(@versionId, 'columnName', @columnName),
+			(@versionId, 'defaultValue', @defaultValue);
+
+		UPDATE CurrentVersion SET CurrentVersion = @versionId;
+	END
+END;
+
+GO
+
+CREATE OR ALTER PROCEDURE RevertCreateDefaultConstraint(
+	@tableName VARCHAR(128),
+	@columnName VARCHAR(128)
+) AS
+BEGIN
+	DECLARE @SQL VARCHAR(MAX);
+	SET @SQL = 'ALTER TABLE ' + @tableName + ' DROP CONSTRAINT DF_' + @tableName + '_' + @columnName;
+	PRINT @SQL;
+	EXEC(@SQL);
+END;
+
+GO
+
+CREATE OR ALTER PROCEDURE AddForeignkeyConstraint(
+	@tableName VARCHAR(128),
+	@columnName VARCHAR(128),
+	@referencedTable VARCHAR(128),
+	@referencedColumn VARCHAR(128),
+	@addToVersionHistory BIT = 1
+) AS
+BEGIN
+	DECLARE @SQL VARCHAR(MAX);
+	SET @SQL = 'ALTER TABLE ' + @tableName + ' ADD CONSTRAINT FK_' + @tableName + '_' + @columnName +
+			   ' FOREIGN KEY (' + @columnName + ') REFERENCES ' + @referencedTable + '(' + @referencedColumn + ')';
+	PRINT @SQL;
+	EXEC(@SQL);
+
+		IF @addToVersionHistory = 1
+	BEGIN
+		INSERT INTO VersionHistory (ProcedureName) VALUES ('AddForeignkeyConstraint');
+		DECLARE @versionId INT = SCOPE_IDENTITY();
+
+		INSERT INTO VersionParams (VersionId, ParamName, ParamValue) VALUES
+			(@versionId, 'tableName', @tableName),
+			(@versionId, 'columnName', @columnName),
+			(@versionId, 'referencedTable', @referencedTable),
+			(@versionId, 'referencedColumn', @referencedColumn);
+
+		UPDATE CurrentVersion SET CurrentVersion = @versionId;
+	END
+END;
+
+GO
+
+CREATE OR ALTER PROCEDURE RevertAddForeignkeyConstraint(
+	@tableName VARCHAR(128),
+	@columnName VARCHAR(128)
+) AS
+BEGIN
+	DECLARE @SQL VARCHAR(MAX);
+	SET @SQL = 'ALTER TABLE ' + @tableName + ' DROP CONSTRAINT FK_' + @tableName + '_' + @columnName;
+	PRINT @SQL;
+	EXEC(@SQL);
+END;
+
+GO
+
+CREATE OR ALTER PROCEDURE GoToVersion(@targetVersion INT)
+AS
+BEGIN
+	DECLARE @currentVersion INT;
+	DECLARE @procedureName VARCHAR(128);
+	DECLARE @tableName VARCHAR(128);
+	DECLARE @columns VARCHAR(128);
+	DECLARE @columnName VARCHAR(128);
+	DECLARE @columnType VARCHAR(128);
+	DECLARE @defaultValue VARCHAR(128);
+	DECLARE @referencedTable VARCHAR(128);
+	DECLARE @referencedColumn VARCHAR(128);
+	DECLARE @oldDataType VARCHAR(128);
+
+	SELECT @currentVersion = CurrentVersion FROM CurrentVersion;
+
+	IF @targetVersion >= 0 AND @currentVersion > @targetVersion
+	BEGIN
+		WHILE @currentVersion > @targetVersion
+		BEGIN
+
+		END
+
+	END
+
+	ELSE IF @currentVersion < @targetVersion AND @targetVersion <= (SELECT MAX(VersionID) FROM VersionHistory)
+		WHILE @currentVersion < @targetVersion
+		BEGIN
+		
+		END
+	END
+
+END;
+
+
+
+
+
 
 EXEC Setup;
