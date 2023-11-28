6eb8b1dc8623e3eecc0140e62e9d75d0de3b213b
Mircea000 <152279162+Mircea000@users.noreply.github.com>
2023-11-28 16:34:11 +0200

    Add procedure to L4/4

--- Lab/Lab4/ex4.sql
+++ Lab/Lab4/ex4.sql
@@ -1,31 +1,42 @@
-ALTER TABLE Arbeiter
-ALTER COLUMN Gehalt DECIMAL(10, 2);
+CREATE PROCEDURE UpdateArbeiterGehalt
+    @ArbeiterId INT,
+    @Erhohungsfaktor DECIMAL(4, 2)
+AS
+BEGIN
+    DECLARE @Gehalt DECIMAL(10, 2);
+    DECLARE @NeuesGehalt DECIMAL(10, 2);
+
+    SELECT @Gehalt = Gehalt
+    FROM Arbeiter
+    WHERE ArbeiterId = @ArbeiterId;
+
+    SET @NeuesGehalt = @Gehalt * @Erhohungsfaktor;
+
+    UPDATE Arbeiter
+    SET Gehalt = @NeuesGehalt
+    WHERE ArbeiterId = @ArbeiterId;
+
+    PRINT 'ArbeiterId: ' + CAST(@ArbeiterId AS VARCHAR(10)) + ' , Neues Gehalt: ' + CAST(@NeuesGehalt AS VARCHAR(20));
+END;
+
+GO;
 
 DECLARE ArbeiterCursor CURSOR FOR
-SELECT ArbeiterId, Gehalt
+SELECT ArbeiterId
 FROM Arbeiter;
 
 OPEN ArbeiterCursor;
 
 DECLARE @ArbeiterId INT;
-DECLARE @Gehalt INT;
-
-FETCH NEXT FROM ArbeiterCursor INTO @ArbeiterId, @Gehalt;
-
 DECLARE @Erhohungsfaktor DECIMAL(4, 2) = 1.10;
+
+FETCH NEXT FROM ArbeiterCursor INTO @ArbeiterId;
 
 WHILE @@FETCH_STATUS = 0
 BEGIN
-	DECLARE @NeuesGehalt DECIMAL(10, 2);
-	SET @NeuesGehalt = @Gehalt * @Erhohungsfaktor;
+    EXEC UpdateArbeiterGehalt @ArbeiterId, @Erhohungsfaktor;
 
-	UPDATE Arbeiter
-	SET Gehalt = @NeuesGehalt
-	WHERE ArbeiterId = @ArbeiterId;
-
-	PRINT 'ArbeiterId: ' + CAST(@ArbeiterId AS VARCHAR(10)) + ' , Neues Gehalt: ' + CAST(@NeuesGehalt AS VARCHAR(20));
-
-	FETCH NEXT FROM ArbeiterCursor INTO @ArbeiterId, @Gehalt;
+    FETCH NEXT FROM ArbeiterCursor INTO @ArbeiterId;
 END
 
 CLOSE ArbeiterCursor;
