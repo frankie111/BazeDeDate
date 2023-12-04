346476b2635f825d2120d6c4dad053c44055f0c7
Mircea000 <152279162+Mircea000@users.noreply.github.com>
2023-12-04 22:05:34 +0200

    Add L5/1

---
+++ Lab/Lab5/ex1.sql
@@ -0,0 +1,63 @@
+CREATE TABLE Ta (
+	idA INT PRIMARY KEY,
+	a2 INT UNIQUE,
+);
+
+DROP TABLE Tc;
+DROP TABLE Ta;
+DROP TABLE Tb;
+
+CREATE TABLE Tb (
+	idB INT PRIMARY KEY,
+	b2 INT,
+	b3 INT
+);
+
+CREATE TABLE Tc (
+	idC INT PRIMARY KEY,
+	idA INT FOREIGN KEY REFERENCES Ta(idA),
+	idB INT FOREIGN KEY REFERENCES Tb(idB)
+);
+
+GO;
+
+CREATE OR ALTER PROCEDURE PopulateTables
+AS
+BEGIN
+    DECLARE @counterA INT = 1;
+
+    WHILE @counterA <= 10000
+    BEGIN
+        INSERT INTO Ta (idA, a2)
+        VALUES (@counterA, @counterA);
+
+        SET @counterA = @counterA + 1;
+    END
+
+    DECLARE @counterB INT = 1;
+
+    WHILE @counterB <= 3000
+    BEGIN
+        INSERT INTO Tb (idB, b2, b3)
+        VALUES (@counterB, @counterB, @counterB + 10000);
+
+        SET @counterB = @counterB + 1;
+    END
+
+    DECLARE @counterC INT = 1;
+
+    WHILE @counterC <= 30000
+    BEGIN
+        DECLARE @idA INT = @counterC % 10000 + 1;
+        DECLARE @idB INT = @counterC % 3000 + 1;
+
+        INSERT INTO Tc (idC, idA, idB)
+        VALUES (@counterC, @idA, @idB);
+
+        SET @counterC = @counterC + 1;
+    END
+END;
+
+EXEC PopulateTables;
+
+SELECT * FROM Tc;
