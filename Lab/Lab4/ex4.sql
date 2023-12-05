5ec350bd7ae98f094bce811961a1f65c30b859b2
Mircox <mircea.rautoiu@gmail.com>
2023-12-05 16:04:26 +0200

    Update L4/4

--- Lab/Lab4/ex4.sql
+++ Lab/Lab4/ex4.sql
@@ -22,6 +22,39 @@ BEGIN
     PRINT 'ArbeiterId: ' + CAST(@ArbeiterId AS VARCHAR(10)) + ' , Neues Gehalt: ' + CAST(@NeuesGehalt AS VARCHAR(20));
 END;
 
+CREATE OR ALTER PROCEDURE UpdateArbeiterGehalt
+    @ArbeiterId INT,
+    @Erhohungsfaktor DECIMAL(4, 2)
+AS
+BEGIN
+    DECLARE @Gehalt DECIMAL(10, 2);
+    DECLARE @NeuesGehalt DECIMAL(10, 2);
+    DECLARE @Position VARCHAR(40);
+
+    SELECT @Gehalt = Gehalt, @Position = Position
+    FROM Arbeiter
+    WHERE ArbeiterId = @ArbeiterId;
+
+    -- Customize increase based on Position
+    DECLARE @IncreaseFactor DECIMAL(4, 2);
+
+    SET @IncreaseFactor = 
+        CASE 
+            WHEN @Position = 'Manager' THEN 1.15
+            WHEN @Position = 'Koch' THEN 1.10
+            ELSE 1.05
+        END;
+
+    SET @NeuesGehalt = @Gehalt * @Erhohungsfaktor * @IncreaseFactor;
+
+    UPDATE Arbeiter
+    SET Gehalt = @NeuesGehalt
+    WHERE ArbeiterId = @ArbeiterId;
+
+    PRINT 'ArbeiterId: ' + CAST(@ArbeiterId AS VARCHAR(10)) + ' , Neues Gehalt: ' + CAST(@NeuesGehalt AS VARCHAR(20));
+END;
+
+
 GO;
 
 select * from Arbeiter;
