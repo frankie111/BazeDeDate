a975f51ba21fab855fc872f5e964b63d7b2d5830
mircozenBalozen <mircea.rautoiu@gmail.com>
2024-01-13 23:03:07 +0200

    Fix s1_3

--- Modele Practic/s1/s1_3.sql
+++ Modele Practic/s1/s1_3.sql
@@ -1,22 +1,11 @@
 SELECT TV.nume, COUNT(STV.spectatorId) AS numarVizionari
-FROM Spectatori AS S
-JOIN SpectatoriTVShows AS STV ON S.spectatorId = STV.spectatorId
+FROM SpectatoriTVShows AS STV
 JOIN TVShows AS TV ON STV.tvShowId = TV.tvShowId
-GROUP BY TV.nume, STV.spectatorId
+GROUP BY TV.nume
 HAVING COUNT(STV.spectatorId) > 
 (
-SELECT COUNT(STV.spectatorId)
-FROM SpectatoriTVShows AS STV
-JOIN TVShows AS TV ON STV.tvShowId = TV.tvShowId
-WHERE TV.nume = 'Megastar'
+	SELECT COUNT(STV.spectatorId)
+	FROM SpectatoriTVShows AS STV
+	JOIN TVShows AS TV ON STV.tvShowId = TV.tvShowId
+	WHERE TV.nume = 'Megastar'
 );
-
-
-SELECT COUNT(STV.spectatorId)
-FROM SpectatoriTVShows AS STV
-JOIN TVShows AS TV ON STV.tvShowId = TV.tvShowId
-WHERE TV.nume = 'Megastar';
-
-SELECT COUNT(STV.spectatorId) AS numarVizionari
-FROM SpectatoriTVShows AS STV
-WHERE 
